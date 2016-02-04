83936106b0faa5fa31b2007d0f34d743d8697254
Xingye Kan <sweetviolin@Xingyes-MacBook-Air.local>
2016-02-04 09:59:08 -0600

    added rangeSum.cpp

---
+++ rangeSum.cpp
@@ -0,0 +1,28 @@
+tor<int> sum;
+
+    NumArray(vector<int> &nums) {
+        
+        int n = nums.size();
+        
+        // in case n = 0
+        sum = vector<int>(n+1,0);
+        
+        
+        
+        
+        for (int i = 0; i < n; ++i){
+            sum[i+1] = sum[i] + nums[i];
+        }
+        
+    }
+    
+    // main
+
+    int sumRange(int i, int j) {
+        
+        if (sum.size() == 0) return 0;
+        
+        return sum[j+1] - sum[i];
+        
+        
+    }

95789d5edf1d3e477e518082d04b2e9d09585a6b
Xingye Kan <sweetviolin@dhcp28.merunorth107.iit.edu>
2016-02-27 12:56:19 -0600

    added firstBadVersion.cpp

---
+++ firstBadVersion.cpp
@@ -0,0 +1,23 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int low = 1, high = n;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if (isBadVersion(mid) && !isBadVersion(mid-1)){
+                return mid;
+            }
+            if (isBadVersion(mid)) {
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+        
+    }
+};
